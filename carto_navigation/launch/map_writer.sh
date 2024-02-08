797d413ca6cfb5836b76e6856e960bd9d6d45487
chy-wsl <1274653465@qq.com>
2024-02-08 14:39:16 +0000

    Separating carto_navigation

--- carto_navigation/launch/map_writer.sh
+++ carto_navigation/launch/map_writer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rosservice call /finish_trajectory 0
-rosservice call /write_state "filename: '/home/sim2real/ep_ws/src/sim2real-XL/carto_navigation/maps/map.pbstream'"
-rosrun cartographer_ros cartographer_pbstream_to_ros_map -map_filestem=/home/sim2real/ep_ws/src/sim2real-XL/carto_navigation/maps/map -pbstream_filename=/home/sim2real/ep_ws/src/sim2real-XL/carto_navigation/maps/map.pbstream -resolution=0.05
+rosservice call /write_state "filename: '/opt/ep_ws/src/rmus_solution/carto_navigation/maps/map.pbstream'"
+rosrun cartographer_ros cartographer_pbstream_to_ros_map -map_filestem=/opt/ep_ws/src/rmus_solution/navigation/maps \
+         -pbstream_filename=/opt/ep_ws/src/rmus_solution/carto_navigation/maps/map.pbstream -resolution=0.05
